59c05b820730a57a00a0c4264f581e4df12c0c79
mk43313@gmail.com <mk43313@gmail.com>
2024-01-06 00:57:10 +0900

    FEAT : LOGIN_DESIGN

--- app/src/main/java/com/example/myhealthybody/MainActivity.kt
+++ app/src/main/java/com/example/myhealthybody/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.myhealthybody
 
+import android.graphics.Point
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_signup)
     }
 }
